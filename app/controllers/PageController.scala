113a241927763ccc7128b946edf626cef91a75a2
David Hollis <dhollis@raybeam.com>
2017-01-18 09:41:04 -0500

    Add forms to controller.

--- app/controllers/PageController.scala
+++ app/controllers/PageController.scala
@@ -5,9 +5,29 @@ import data.PageRepository
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 @Singleton
 class PageController @Inject() (pages: PageRepository) extends Controller {
+  case class PageForm(content: String, id: Option[String] = None)
+
+  val createForm = Form(
+    mapping(
+      "id" -> nonEmptyText,
+      "content" -> text
+    ){
+      (id: String, content: String) => PageForm(content, Some(id))
+    }{
+      (form: PageForm) => form.id.map { id => (id, form.content) }
+    }
+  )
+
+  val updateForm = Form(
+    mapping(
+      "content" -> text
+    )(PageForm(_, None))((form: PageForm) => Some(form.content))
+  )
 
   def home = Action { implicit request =>
     Redirect(routes.PageController.page("home"))
